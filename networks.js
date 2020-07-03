d094185e34f66047653c73a794114b5424a4a11b
zhutianchi <zhutianchi@mcarlo.com>
2020-07-03 23:07:48 +0800

    mainnet

--- networks.js
+++ networks.js
@@ -1,3 +1,5 @@
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
@@ -8,5 +10,12 @@ module.exports = {
       gasPrice: 5e9,
       networkId: '*',
     },
+    man: {
+        network_id: 1,
+        gas: 1000000,
+        gasPrice: 40*1000000000,
+        provider: function () { return new HDWalletProvider('', 'http://server10.jy.mcarlo.com:8645'); },
+        networkCheckTimeout: 30000,
+    }
   },
 };
