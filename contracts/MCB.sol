55c249bb44a64f5783cd819f26132efb52dc975e
zhutianchi <zhutianchi@mcarlo.com>
2020-07-03 22:56:44 +0800

    erc20

---
+++ contracts/MCB.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/presets/ERC20PresetMinterPauser.sol";
+
+/**
+ * @dev MCB token
+ */
+contract MCB is ERC20PresetMinterPauserUpgradeSafe {
+
+}
